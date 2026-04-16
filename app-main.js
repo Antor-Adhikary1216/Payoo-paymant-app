1b810d7ffe2db330d3f00fde109a6bd0fdcb3e1f
Antor-Adhikary1216 <antoradhikarygithub@gmail.com>
2026-04-16 09:39:35 +0530

    toggling function

--- app-main.js
+++ app-main.js
@@ -20,3 +20,63 @@ document.getElementById("add-mony-btn").addEventListener("click",
        
     }
 )
+
+// withdraw Button function 
+document.getElementById("withdraw-btn").addEventListener("click",function(e){
+    e.preventDefault()
+    console.log("withrolw button is working")
+    const amountValue =parseInt( document.getElementById("amount-value").value);
+    const abelaveLBalince = parseInt(document.getElementById("abelavel-balince").innerText);
+   const totalMinace =  abelaveLBalince- amountValue;
+   document.getElementById("abelavel-balince").innerText = totalMinace;
+
+})
+
+
+
+
+// toggling function here <========>
+     document.getElementById("add-money").addEventListener("click",
+    function(){
+      document.getElementById("cash-out-parente-div").style.display = "none"
+       document.getElementById("transfer-money-parent-div").style.display = "none"
+       document.getElementById("get-bouns-main-div").style.display = "none"
+      document.getElementById("add-money-prente-div").style.display  = "block"
+    }
+  )
+  document.getElementById("cash-out").addEventListener("click",function(){
+    document.getElementById("add-money-prente-div").style.display = "none";
+     document.getElementById("transfer-money-parent-div").style.display = "none"
+      document.getElementById("main-Pay-bill-div").style.display = "none"
+        document.getElementById("get-bouns-main-div").style.display = "none"
+    document.getElementById("cash-out-parente-div").style.display= "block";
+  })
+
+  document.getElementById("Money-transfer").addEventListener("click",function(){
+    document.getElementById("add-money-prente-div").style.display = "none"
+    document.getElementById("cash-out-parente-div").style.display = "none"
+     document.getElementById("main-Pay-bill-div").style.display = "none"
+       document.getElementById("get-bouns-main-div").style.display = "none"
+    document.getElementById("transfer-money-parent-div").style.display = "block"
+
+  })
+
+  document.getElementById("pay-bills-div").addEventListener("click",function(){
+     document.getElementById("add-money-prente-div").style.display = "none"
+    document.getElementById("cash-out-parente-div").style.display = "none"
+    document.getElementById("transfer-money-parent-div").style.display = "none"
+      document.getElementById("get-bouns-main-div").style.display = "none"
+    document.getElementById("main-Pay-bill-div").style.display = "block"
+
+  })
+
+  document.getElementById("get-bouns").addEventListener("click",function(){
+      document.getElementById("add-money-prente-div").style.display = "none"
+    document.getElementById("cash-out-parente-div").style.display = "none"
+    document.getElementById("transfer-money-parent-div").style.display = "none"
+    document.getElementById("main-Pay-bill-div").style.display = "none"
+    document.getElementById("get-bouns-main-div").style.display = "block"
+  })
+
+
+// toggling functin over here>======<
